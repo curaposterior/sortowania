b6c3fb4cdcdb99afe323631ff22d565071231664
curaposterior <empo123@interia.pl>
2021-01-13 23:35:19 +0100

    dodanie sortowania babelkowego

---
+++ sortowania_inne/sortowanie_babelkowe.py
@@ -0,0 +1,18 @@
+#dwa sposoby na postortowanie tablicy algorytmem sortowania bąbelkowego
+
+def bubble1(T):
+    n = len(T)
+    for j in range(n - 1, 0, -1):
+        for i in range(i):
+            if T[i] < T[i + 1]:
+                T[i], T[i + 1] = T[i + 1], T[i]
+    return T
+
+def bubble2(tab):
+    for i in range(len(tab) - 1):
+        for j in range(len(tab) - 1):
+            if tab[j] > tab[j + 1]:
+                temp = tab[j]
+                tab[j] = tab[j + 1]
+                tab[j + 1]= temp
+    return tab    
