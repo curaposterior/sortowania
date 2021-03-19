c754ef1c11cf0ad1fb8cdf0b6dfde37c95860bf8
curaposterior <64332527+curaposterior@users.noreply.github.com>
2021-03-19 08:46:35 +0100

    Update
    Zmieniona zmienna sterująca (był błąd)

--- sortowania_inne/sortowanie_babelkowe.py
+++ sortowania_inne/sortowanie_babelkowe.py
@@ -3,7 +3,7 @@
 def bubble1(T):
     n = len(T)
     for j in range(n - 1, 0, -1):
-        for i in range(i):
+        for i in range(j):
             if T[i] < T[i + 1]:
                 T[i], T[i + 1] = T[i + 1], T[i]
     return T
